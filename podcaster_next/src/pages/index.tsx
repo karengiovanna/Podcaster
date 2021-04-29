5589664534533f5118d796871b0c9a594921879d
Karen Giovanna <karengiovnn@gmail.com>
2021-04-29 16:57:17 -0400

    Adicionei o axios

--- podcaster_next/src/pages/index.tsx
+++ podcaster_next/src/pages/index.tsx
@@ -3,28 +3,31 @@
 // SSG
 
 import {GetStaticProps} from 'next';
+import { api } from '../services/api';
 // import { api } from '../services/api';
 // import {format, parseISO} from 'date-fns'; //converte data em string para date em js
 // import ptBR from 'date-fns/locale/pt-BR'; // data em português
 // import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
 
-// type Episode = {
-//   id: string;
-//   title: string;
-//   thumbnail: string;
-//   description: string;
-//   url: string;
-//   members: string;
-//   published_at: string;
-//   //....
-// }
+type Episode = {
+   d: string;
+  title: string;
+  thumbnail: string;
+  description: string;
+  url: string;
+  members: string;
+  published_at: string;
+ //....
+}
 
-// type HomeProps = {
-//   episodes: Episode[]; //um array de episódios
-// }
+type HomeProps = { //tipagem das props
+  episodes: Episode[]; //um array de objeto
+}
 
-export default function Home(props) {
+
+
+export default function Home(props: HomeProps) { //Homeprops é a tipagem de um episodio
   return (
     <div>
       <h1>Index</h1>
@@ -34,9 +37,15 @@ export default function Home(props) {
 }
 
 
-export const getStaticProps: GetStaticProps = async() =>{ 
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json()
+export const getStaticProps: GetStaticProps = async() =>{ //tanto os parâmetros da função quanto o retorno estão tipados
+  const {data} = await api.get('episodes', {
+    params:{
+      _limit: 12,
+      _sort: 'publishes_at',
+      _order: 'desc'
+  }
+})
+
 
   return{
     props:{ // conceito de propriedades do react
