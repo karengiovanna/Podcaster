ba1cbfef6a1a92de33dec81eb74efbef1df2a636
Karen Giovanna <karengiovnn@gmail.com>
2021-04-30 14:01:47 -0400

    Objectfit no Image

--- podcaster_next/src/pages/index.tsx
+++ podcaster_next/src/pages/index.tsx
@@ -47,6 +47,7 @@ export default function Home({latestEpisodes, allEpisodes}: HomeProps) { //Homep
                       height={192} 
                       src={episode.thumbnail} 
                       alt={episode.title}
+                      objectFit="cover"
                     /> 
                     
 
