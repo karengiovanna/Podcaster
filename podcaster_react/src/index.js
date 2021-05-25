dc6511e60f1aafe9d13a61b8b2a9ea8ae9ee7401
Karen Giovanna <giovannagiovanna13@gmail.com>
2021-05-25 17:46:10 -0400

    Delete All

--- podcaster_react/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom'; /* integração do react com o browser, representação do html como formato de objeto do javascript*/
-import App from './App'; /* importamos o outro arquivo*/
-
-ReactDOM.render( /* renderizar é o ato de mostrar em tela*/
-    <App />, /* mostra em tela o app. app é uma funcao que retorna html*/
-  document.getElementById('root') /* mostra o app dentro do elemento root no html*/
-);
