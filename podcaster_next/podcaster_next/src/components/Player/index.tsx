70f9a91000c22f707a78332dbc25e877bc368165
Karen Giovanna <giovannagiovanna13@gmail.com>
2021-05-30 13:02:12 -0400

    Configurando opacidade dos botões ao tocar podcast

--- podcaster_next/podcaster_next/src/components/Player/index.tsx
+++ podcaster_next/podcaster_next/src/components/Player/index.tsx
@@ -36,7 +36,7 @@ export default function Player(){
                 </div>
             )}
 
-            <footer className={styles.empty}>
+            <footer className={!episode ? styles.empty : ''}>
                 <div className={styles.progress}>
                     <span>00:00</span>
 
