78409a720a8bb9612e9edb49fec0c52e9cf40bee
Karen Giovanna Melo <49700354+karengiovanna@users.noreply.github.com>
2021-07-11 18:09:19 -0400

    Upload files to root

--- app_react/podcaster/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom'; //biblioteca de integração do react com o navegador (representação do html em formato de objeto javascript)
-import App from './App';
-
-ReactDOM.render( //quero mostrar em tela(renderizar)
-  <App />,
-  document.getElementById('root') //quero mostrar o app dentro elemento do id root
-);
